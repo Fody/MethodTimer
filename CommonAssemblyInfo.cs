0923c49c6aaa2f6b3b9c23143d21560844b36f8a
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 20:06:10 +1000

    support for TFS checkout

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("MethodTimer")]
 [assembly: AssemblyProduct("MethodTimer")]
-[assembly: AssemblyVersion("1.11.2.0")]
-[assembly: AssemblyFileVersion("1.11.2.0")]
+[assembly: AssemblyVersion("1.11.3.0")]
+[assembly: AssemblyFileVersion("1.11.3.0")]

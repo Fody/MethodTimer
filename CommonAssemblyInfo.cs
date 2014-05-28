d66d8f630c28b6b6e884853b78ed9cf73c248b21
Simon Cropp <simon.cropp@gmail.com>
2014-05-28 15:03:39 +1000

    better pcl support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("MethodTimer")]
 [assembly: AssemblyProduct("MethodTimer")]
-[assembly: AssemblyVersion("1.14.1")]
-[assembly: AssemblyFileVersion("1.14.1")]
+[assembly: AssemblyVersion("1.14.2")]
+[assembly: AssemblyFileVersion("1.14.2")]

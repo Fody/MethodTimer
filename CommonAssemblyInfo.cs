3ef5e5a16184b1380065e698b5e12d350215ca7b
Simon Cropp <simon.cropp@gmail.com>
2016-02-13 13:47:38 +1100

    remove msbuild hack

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("MethodTimer")]
 [assembly: AssemblyProduct("MethodTimer")]
-[assembly: AssemblyVersion("1.15.9")]
-[assembly: AssemblyFileVersion("1.15.9")]
+[assembly: AssemblyVersion("1.15.10")]
+[assembly: AssemblyFileVersion("1.15.10")]

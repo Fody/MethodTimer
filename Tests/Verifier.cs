71cd7694aa2f4d89956017ca3a7277144a29c5f5
Simon Cropp <simon.cropp@gmail.com>
2017-11-17 16:06:27 +1100

    redundant ref

--- Tests/Verifier.cs
+++ Tests/Verifier.cs
@@ -1,11 +1,30 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
-using Microsoft.Build.Utilities;
 using NUnit.Framework;
 
 public static class Verifier
 {
+    static string exePath;
+
+    static Verifier()
+    {
+        var windowsSdk = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\");
+        exePath = Directory.EnumerateFiles(windowsSdk, "PEVerify.exe", SearchOption.AllDirectories)
+            .OrderBy(x =>
+            {
+                var fileVersionInfo = FileVersionInfo.GetVersionInfo(x);
+                return new Version(fileVersionInfo.FileMajorPart, fileVersionInfo.FileMinorPart, fileVersionInfo.FileBuildPart);
+            })
+            .FirstOrDefault();
+        if (exePath == null)
+        {
+            throw new Exception("Could not find path to PEVerify");
+        }
+    }
+
     public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
     {
         var before = Validate(beforeAssemblyPath);
@@ -16,25 +35,16 @@ public static class Verifier
 
     static string Validate(string assemblyPath2)
     {
-        var exePath = GetPathToPeVerify();
-        if (!File.Exists(exePath))
-        {
-            return string.Empty;
-        }
-        var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
+        using (var process = Process.Start(new ProcessStartInfo(exePath, $"\"{assemblyPath2}\"")
         {
             RedirectStandardOutput = true,
             UseShellExecute = false,
             CreateNoWindow = true
-        });
-
-        process.WaitForExit(10000);
-        return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
-    }
-
-    static string GetPathToPeVerify()
-    {
-        return ToolLocationHelper.GetPathToDotNetFrameworkFile("peverify.exe",TargetDotNetFrameworkVersion.VersionLatest);
+        }))
+        {
+            process.WaitForExit(10000);
+            return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+        }
     }
 
     static string TrimLineNumbers(string foo)

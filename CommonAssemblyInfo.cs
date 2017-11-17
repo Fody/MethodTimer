20339a579fcb05448cb58c6dc7232429e6e4e7e0
Simon Cropp <simon.cropp@gmail.com>
2017-11-17 19:23:37 +1100

    fix nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("MethodTimer")]
 [assembly: AssemblyProduct("MethodTimer")]
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]

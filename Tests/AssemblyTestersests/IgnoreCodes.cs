46e321bd18d0038d41414874974380a25bd68784
Simon Cropp <simon.cropp@gmail.com>
2018-01-14 16:37:02 +1100

    fix ref

--- Tests/AssemblyTestersests/IgnoreCodes.cs
+++ Tests/AssemblyTestersests/IgnoreCodes.cs
@@ -5,7 +5,7 @@ public static class IgnoreCodes
     public static IEnumerable<string> GetIgnoreCoders()
     {
 #if NET461
-        return Enumerable.Empty<string>();
+        return System.Linq.Enumerable.Empty<string>();
 #endif
 #if NETCOREAPP2_0
         return new[] { "0x80131869" };

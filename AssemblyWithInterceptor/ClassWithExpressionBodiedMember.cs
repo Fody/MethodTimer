53f9a63161ab3c882850be887f401de15cf440bf
Simon Cropp <simon.cropp@gmail.com>
2019-04-02 00:23:30 +1100

    Reverse fix returns (#79)

---
+++ AssemblyWithInterceptor/ClassWithExpressionBodiedMember.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using MethodTimer;
+
+[Time]
+public class ClassWithExpressionBodiedMember
+{
+    public void Method()
+    {
+        var fooBar = FooBar;
+        Trace.WriteLine(fooBar);
+    }
+
+    public bool FooBar => Inner != null && Inner.Foobar;
+    public Bar Inner;
+
+    public class Bar
+    {
+        public bool Foobar;
+    }
+}

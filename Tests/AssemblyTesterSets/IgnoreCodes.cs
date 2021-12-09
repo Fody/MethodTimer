3687a4fa639e1dcb3d9d37562d86e511113439af
Simon Cropp <simon.cropp@gmail.com>
2021-12-10 08:37:24 +1100

    Update IgnoreCodes.cs

--- Tests/AssemblyTesterSets/IgnoreCodes.cs
+++ Tests/AssemblyTesterSets/IgnoreCodes.cs
@@ -4,11 +4,6 @@ public static class IgnoreCodes
 {
     public static IEnumerable<string> GetIgnoreCoders()
     {
-#if NET472
-        return System.Linq.Enumerable.Empty<string>();
-#endif
-#if NETCOREAPP2_2
         return new[] { "0x80131869" };
-#endif
     }
 }
